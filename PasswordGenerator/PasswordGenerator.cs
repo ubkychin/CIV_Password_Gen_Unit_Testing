76b5e7d761818305bcb39dbf829a83fa71ddd394
Anh Nguyen <anguyen@swin.edu.au>
2019-08-19 13:42:30 +1000

    Initial commit

---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+        public class PasswordGenerator {
+
+        public string PasswordGenOne(string firstname, string lastname, int age) {
+            throw new NotImplementedException();
+        }
+
+        public string PasswordGenTwo(string firstname, string lastname, int age) {
+            throw new NotImplementedException();
+        }
+
+        public string PasswordGenThree(int length) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PasswordGeneratorTests/PasswordGeneratorTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace Tests {
+    public class Tests {
+        [SetUp]
+        public void Setup () {}
+
+        [Test]
+        public void PasswordGenOneTest (string firstname, string lastname, int age, string ans) {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void PasswordGenTwoTest (string firstname, string lastname, int age, string ans) {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void PasswordGenThreeTest(int length) {
+            Assert.Fail();
+        }
+    }
+}
